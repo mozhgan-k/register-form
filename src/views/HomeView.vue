e7c6029e3d22ea6d666b1a03a82a1a2fd5768c75
Mozhgan <mozhan.kabki@gmail.com>
2025-02-02 14:29:28 +0330

    Update: HomeView.vue => add description component

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
 import Wizard from "@/components/Wizard.vue";
+// import Description from "@/components/Description.vue";
 </script>
 
 <template>
   <main style="max-width: 900px; margin-inline: auto;">
     <Wizard/>
+<!--    <br>-->
+<!--    <Description/>-->
   </main>
 </template>
